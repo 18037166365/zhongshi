b92d7531291e63edd5f9517e43d20934dcc592d2
peien <peien@iyenei.com>
2019-01-15 10:55:46 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import FastClick from 'fastclick'
+import VueRouter from 'vue-router'
+import App from './App'
+import Home from './components/HelloFromVux'
+import  { AlertPlugin } from 'vux'
+
+Vue.use(VueRouter)
+Vue.use(AlertPlugin)
+
+const routes = [{
+  path: '/',
+  component: Home
+}]
+
+const router = new VueRouter({
+  routes
+})
+
+FastClick.attach(document.body)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app-box')
