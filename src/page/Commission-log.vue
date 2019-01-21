1215b312e3dc37ae86a6a2e03b531dc3ea47d7f0
peien <peien@iyenei.com>
2019-01-21 09:09:50 +0800

    log

--- src/page/Commission-log.vue
+++ src/page/Commission-log.vue
@@ -1,120 +1,169 @@
 <template>
-  <div class="main-wrap">
-    <div class="header">
-      <div>我的销售额 ¥ {{ salse }}</div>
-      <div>我的佣金额 ¥ {{ rebate }}</div>
-    </div>
-    <div class="log-wrap">
-      <div class="log-item" v-for="item in logList" :key="item.id">
-        <div class="top">
-          <div class="left">获得 {{ item.fromuser_classtext }}</div>
-          <div class="mid">
-            <img :src="item.fromuser_headimguri" alt="" class="head">
-            <div class="nick">{{ item.fromuser_name }}</div>
-          </div>
-          <div class="right">
-            <div class="money">
-              <div>的分佣</div>
-              <div class="detail"> ¥ {{ item.money }} </div>
+    <div class="main-wrap">
+        <div class="header">
+            <div>我的销售额 ¥ {{ salse }}</div>
+            <div>我的佣金额 ¥ {{ rebate }}</div>
+        </div>
+        <div class="log-wrap" ref="list">
+            <div class="log-item" v-for="item in logList" :key="item.id">
+                <div class="top">
+                    <div class="left">获得 {{ item.fromuser_classtext }}</div>
+                    <div class="mid">
+                        <img :src="item.fromuser_headimguri" alt="" class="head">
+                        <div class="nick">{{ item.fromuser_name }}</div>
+                    </div>
+                    <div class="right">
+                        <div class="money">
+                            <div>的分佣</div>
+                            <div class="detail"> ¥ {{ item.money }} </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="bottom">
+                    {{ item.ctime }}
+                </div>
             </div>
-          </div>
         </div>
-        <div class="bottom">
-         {{ item.ctime }}
-        </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
-import { getRebateLog } from '../api/index.js'
-
-  export default {
-    data() {
-      return {
-        rebate: '',
-        salse: '',
-        logList: [],
-        page: 2
-      }
-    },
-    mounted() {
-      getRebateLog({page: this.page}).then(res => {
-        this.rebate= res.data.rebate
-        this.salse= res.data.salse
-        this.logList= res.data.data
-      })
-
-      setTimeout(() => {
-        this.$vux.loading.show({
-         text: 'Loading'
-        })
-      }, 1000);
+    import { getRebateLog } from '../api/index.js'
+    const PAGE_SIZE = 10;
+    export default {
+        data() {
+            return {
+                rebate: '',
+                salse: '',
+                logList: [],
+                canBePullDown: true
+            }
+        },
+        created() {
+            this.height = document.documentElement.clientHeight;
+            this.page = 1;
+            this._getList().then(respond => {
+                if (!respond) return
+                this.rebate = respond.rebate;
+                this.salse = respond.salse;
+                this.logList = respond.data
+            })
+        },
+        mounted() {
+            document.addEventListener('scroll', this._scrollFn)
+        },
+        methods: {
+            _getList() {
+                return getRebateLog({page: this.page}).then(res => {
+                    if (res.code === 0) {
+                        let result = res.data;
+                        if (result.data.length) {
+                            if (result.data.length >= PAGE_SIZE) {
+                                this.canBePullDown = true
+                            }
+                        }
+                        return res.data
+                    }
+                })
+            },
+           _scrollFn() {
+                let orderDom = this.$refs.list;
+                let boxHeight = orderDom.clientHeight;
+                console.log('-----------------------------------------------------------------------------------------');
+                console.log('boxHeight: ', boxHeight);
+                let scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+                console.log('scrollTop: ', scrollTop);
+                let offset = boxHeight + orderDom.offsetTop - scrollTop - this.height;
+                console.log('offset: ', offset);
+                console.log('this.height: ', this.height);
+                console.log('scrollTop: ', scrollTop);
+                console.log('orderDom.offsetTop: ', orderDom.offsetTop);
+                console.log('boxHeight: ', boxHeight);
+                if (offset <= 0 && this.canBePullDown) {
+                    this.canBePullDown = false;
+                    this.page++;
+                    this._getList().then(respond => {
+                        if (!respond) return;
+                        this.logList = [...this.logList, ...respond]
+                    })
+                }
+            },
+        },
+        beforeRouteLeave(to, from, next) {
+            document.removeEventListener('scroll', this._scrollFn);
+            next()
+        }
     }
-  }
 </script>
 
 <style lang="scss" scoped>
-.main-wrap {
-  font-size: 14px;
-  color: #101010;
-  .header {
-    padding: 14px 16px;
-    display: flex;
-    justify-content: space-between;
-    background: #fff;
-  }
-  .log-wrap {
-    margin-top: 10px;
-    background: #fff;
-    .log-item {
-      display: flex;
-      flex-direction: column;
-      justify-content: flex-start;
-      align-items: flex-start;
-      padding: 14px 16px;
-      @include  b1px;
-      .bottom {
-        display: flex;
-        justify-content: flex-end;
-        width: 100%;
-      }
-      .top {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        width: 100%;
-        .mid {
-          display: flex;
-          justify-content: space-around;
-          align-items: center;
-          img {
-            width: 30px;
-            height: 30px;
-            border-radius: 50%;
-            margin-right: 10px;
-          }
-        }
-        .right {
-          .money {
-            display: flex;
-            justify-content: space-around;
-            .detail {
-              padding-left: 20px;
-            }
-          }
-        }
-      }
-    }
-  }
-}
-</style>
+    .main-wrap {
+        font-size: 14px;
+        color: #101010;
 
-<style>
-  .vux-loading .weui-toast {
-    width: 30% !important;
-    height: auto !important;
-    min-height: 100px !important;
-  }
+        .header {
+            position: fixed;
+            left:0;
+            top: 0;
+            right: 0;
+            padding: 14px 16px;
+            display: flex;
+            justify-content: space-between;
+            background: #fff;
+            z-index: 1;
+            @include b1pxNoP();
+        }
+
+        .log-wrap {
+            margin-top: 45px;
+            background: #fff;
+
+            .log-item {
+                display: flex;
+                flex-direction: column;
+                justify-content: flex-start;
+                align-items: flex-start;
+                padding: 14px 16px;
+                @include b1px;
+
+                .bottom {
+                    display: flex;
+                    justify-content: flex-end;
+                    width: 100%;
+                    height: 300px;
+                }
+
+                .top {
+                    display: flex;
+                    justify-content: space-between;
+                    align-items: center;
+                    width: 100%;
+
+                    .mid {
+                        display: flex;
+                        justify-content: space-around;
+                        align-items: center;
+
+                        img {
+                            width: 30px;
+                            height: 30px;
+                            border-radius: 50%;
+                            margin-right: 10px;
+                        }
+                    }
+
+                    .right {
+                        .money {
+                            display: flex;
+                            justify-content: space-around;
+
+                            .detail {
+                                padding-left: 20px;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 </style>
