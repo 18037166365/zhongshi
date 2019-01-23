b2831bfc6e29fd5badf3232ba2af5b32991c8c94
peien <peien@iyenei.com>
2019-01-23 21:47:55 +0800

    delete

--- src/components/HelloFromVux.vue
+++ src/components/HelloFromVux.vue
@@ -1,14 +1,6 @@
 <template>
   <div>
-    <div class="vux-demo">
-      <img class="logo" src="../assets/vux_logo.png">
-      <h1> </h1>
-    </div>
-    <group title="cell demo">
-      <cell title="VUX" value="cool" is-link></cell>
-    </group>
     <div class="box">
-
     </div>
   </div>
 </template>
